97e2b1c368f42cb13923f170c1ceeaaaed63a3d2
Mampuru <mampuruntwampe@gmail.com>
2023-12-28 23:51:19 +0200

    Addition of the typeDefs that define the schema

---
+++ MERN-book-search-engine/server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+
+/*
+ typeDefs (Type Definitions) are used to define the schema for the API.
+ They describe the types of data available, including object types,
+fields on those types, and the relationships between them. 
+Essentially, typeDefs specify what queries, mutations, and
+subscriptions can be made and what data can be retrieved or modified.
+*/ 
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
