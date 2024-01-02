888d5867019436aef821974b66a443995473faee
Mampuru <mampuruntwampe@gmail.com>
2024-01-02 21:12:12 +0200

    Addition of the function to load contract and account

--- ethereum-todo-app/src/funcs.js
+++ ethereum-todo-app/src/funcs.js
@@ -25,3 +25,17 @@ const loadTasks = async (todoContract, addressAccount) => {
     }
     return tasks;
 };
+
+const loadContract = async (addressAccount) => {
+    const theContract = contract(TodoListJSON);
+    theContract.setProvider(web3.eth.currentProvider);
+    const todoContract = await theContract.deployed();
+    const tasks = await loadTasks(todoContract, addressAccount);
+
+    return { todoContract, tasks }
+};
+
+const loadAccount = async () => {
+    const addressAccount = await web3.eth.getCoinbase();
+    return addressAccount;
+};
