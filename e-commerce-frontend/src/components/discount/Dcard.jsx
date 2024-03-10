a8587bafeb801a0ca73b1ce81dd121e6df6d2900
Mampuru <mampuruntwampe@gmail.com>
2024-03-10 23:32:26 +0200

    Fixed the style sheet import on the discount card component

--- e-commerce-frontend/src/components/discount/Dcard.jsx
+++ e-commerce-frontend/src/components/discount/Dcard.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Ddata from "./Ddata"
-import "../newarrivals/style.css"
+import "../newarrivals/styles.css"
 
 const Dcard = () => {
   const settings = {
