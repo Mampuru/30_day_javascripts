d8504dbe6884f300bfce567ac51bed725bc05a3b
Mampuru <mampuruntwampe@gmail.com>
2024-03-10 23:29:38 +0200

    Fixed the annoucument import issue

--- e-commerce-frontend/src/pages/Pages.jsx
+++ e-commerce-frontend/src/pages/Pages.jsx
@@ -5,8 +5,8 @@ import TopCate from "../components/top/TopCate"
 import NewArrivals from "../components/newarrivals/NewArrivals"
 import Discount from "../components/discount/Discount"
 import Shop from "../components/shops/Shop"
-import Annocument from "../components/annocument/Annocument"
 import Wrapper from "../components/wrapper/Wrapper"
+import Annocument from "../components/annocumnet/Annocument"
 
 const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
   return (
