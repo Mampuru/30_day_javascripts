89e389677a07c551e4330de6c0ac40fd51033ee4
Mampuru <mampuruntwampe@gmail.com>
2024-04-13 01:00:15 +0200

    Fixed the calculation for alum rails

--- get-off-grid-app/src/App.js
+++ get-off-grid-app/src/App.js
@@ -17,10 +17,12 @@ function App() {
       numStrings,
       orientation,
     };
-    setResult(inputData);
+    
     const endClamps = calEndClamps(numStrings);
-    setEndClamp(endClamps);
     const rails = calRails();
+
+    setResult(inputData);
+    setEndClamp(endClamps);
     setRails(rails)
   };
 
@@ -48,11 +50,16 @@ function App() {
           return parseInt(numPanels, 10) + 1;
         }
       } else if (orientation === "Portrait") {
-        //NEED FIXING TUNE ME 
         if (numPanels % 4 === 0) {
-          return numPanels;
-        } else {
-          return numPanels + 1;
+          return numPanels/2;
+        } else if (numPanels % 4 === 2 || numPanels % 4 === 3) {
+          let x = Math.round(numPanels / 4);
+          return x * 2;
+        } else if (numPanels % 4 === 1) {
+          let y = Math.round(numPanels / 4 * 2);
+          return y + 1;
+        }else {
+        
         }
       }
     }
@@ -160,7 +167,7 @@ function App() {
           <h3>No. Components</h3>
           <p>{rail} x  Rails</p>
           <p>{endClamp} x  End Clamps</p>
-          <p>{result.clientName} x  Center Clamps</p>
+          <p>{result.clientName} x  Centre Clamps</p>
           <p>{result.clientName} x  Splices</p>
           <p>{result.clientName} x  Roof Hooks</p>
           <p>{result.numPanels} x  Panels</p>
